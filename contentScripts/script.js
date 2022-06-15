97c20ce36b5118160179da6fbd4b8e14477c2bdd
Seongkwon Min <qkqhekqkqh@korea.ac.kr>
2022-06-15 20:37:36 +0900

    commit

--- contentScripts/script.js
+++ contentScripts/script.js
@@ -1,43 +1,38 @@
-function run() {
-    var list = document.getElementsByClassName("user-nick-text");
-    console.log("first idx : ", list.length);
-
-    if (list.length < 1) return;
-
-    //ban = localStorage.getItem(ban);  미구현
-    let ban = ["Mamette", ]        //*****여기에 닉네임을 입력하세요*****
+function board() {
+    let list = document.getElementsByClassName("tit");
+    console.log("before cleaning : ", list.length);
 
     for (var i = 0; i < list.length; i++) {
-        for (var j = 0; j < ban.length; j++) {
-            if (list[i].innerText == ban[j]) {
-                list[i].parentElement.parentElement.parentElement.parentElement.parentElement.remove();
-                i--;
-                break;
-            }
-            if (list[i].innerText == " " + ban[j]) {
-                list[i].parentElement.parentElement.parentElement.remove();
-                i--;
-                break;
-            }
+        if (list[i].innerText == "차단된 회원 게시물입니다.") {
+            list[i].parentElement.parentElement.remove();
+            i--;
         }
     }
-    console.log("last idx : ", list.length);
+
+    console.log("After cleaning : ", list.length);
 }
 
-/*  미구현
-function push(str) {
-    ban = localStorage.getItem(ban);
-    ban.push(str);
-    localStorage.setItem("ban", ban);
-}
+function cmt() {
+    const itv = setInterval(() => {
+        let list = document.getElementsByClassName("note-editor content-view-ok");
+        if (list.length){
+            clearInterval(itv);
+            console.log("before cleaning : ", list.length);
 
-function pop(num) {
-    ban = localStorage.getItem(ban);
-    ban.splice(num - 1, 1);
-    localStorage.setItem("ban", ban);
+            for (var i = 0; i < list.length; i++) {
+                if (list[i].innerText == "차단된 회원 댓글입니다.") {
+                    list[i].parentElement.parentElement.parentElement.remove();
+                    i--;
+                }
+            }
+
+            console.log("After cleaning : ", list.length);
+        }
+    }, 100);
 }
-*/
 
 window.addEventListener("DOMContentLoaded", () => {
-    run();
-  });
+    board();
+    cmt();
+});
+
